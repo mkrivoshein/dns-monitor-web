817763698fef376ba143225f205980085c7baeec
AlisaPekareva <69_alisa@list.ru>
2022-08-09 23:35:28 +0200

    #8 Размеры таблицы, отступы + размер шрифта футер

--- src/App.svelte
+++ src/App.svelte
@@ -65,7 +65,7 @@
 </svelte:head>
 
 <main>
-    <section class="relative block px-4 pt-4 pb-8 h-300-px">
+    <section class="relative block px-4 pt-4 pb-8  h-300-px">
         <h1 class="py-4 font-bold text-4xl text-blueGray-900">Query DNS records</h1>
         <form on:submit|preventDefault={() => fetchData()}>
         <div class="flex flex-row">
@@ -87,8 +87,8 @@
                 </div>
                 <div class="text-center  py-6 ml-4 sm:py-6 w-fit">
                     <button on:click={() => fetchData()}
-                            class="bg-blueGray-100 text-blueGrey-900 active:bg-blueGray-900 text-sm lg:text-xl font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                            type="button">
+                        class="bg-blueGray-100 text-blueGrey-900 active:bg-blueGray-900 text-sm lg:text-xl font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                        type="button">
                         Report
                     </button>
                 </div>
@@ -98,7 +98,7 @@
         {/if}
         </form>
     </section>
-    <section class=" relative px-3 py-1">
+    <section class="relative block sm:mx-3 px-3 py-1 ">
         <div class="flex flex-wrap mt-1">
             <div class="w-full mb-2">
                 <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded">
@@ -112,15 +112,15 @@
                         </div>
                     </div>
                 </div>
-                 <div class = "container mx-auto 	w-40">
+                
                 <div class= "block w-full py-1  overflow-x-auto ">
-                    <table class=" table-cell  items-center w-full  text-xs lg:text-lg break-words  bg-transparent border border-solid border-collapse  border-x-0 border-y-0">
+                    <table class=" table-row  lg:table-auto  items-center w-full text-xs lg:text-lg whitespace-normal break-all  bg-transparent border border-solid border-collapse  border-x-0 border-y-0">
                         <thead>
                         <tr class="border bg-blueGray-200" > 
                       
-                            <th class="px-0.4 w-screen  py-3 lg:text-lg align-middle uppercase    break-words font-semibold text-left">Subdomain</th>
-                            <th class="px-0.4 w-screen py-3 lg:text-lg align-middle uppercase  break-words  font-semibold text-left">Type</th>
-                            <th class="px-0   w-screen  py-3 lg:text-lg align-middle uppercase  break-words font-semibold text-left">Content</th>
+                            <th class="px-0.4 w-screen  py-3 lg:text-lg align-middle uppercase  whitespace-normal   break-all font-semibold text-left">Subdomain</th>
+                            <th class="px-0.4 w-screen py-3 lg:text-lg align-middle uppercase whitespace-normal  break-all  font-semibold text-left">Type</th>
+                            <th class="px-0   w-screen  py-3 lg:text-lg align-middle uppercase whitespace-normal  break-all font-semibold text-left">Content</th>
                         </tr>
                         </thead >
                         {#each subDomains as subDomain}
@@ -129,7 +129,7 @@
                     </table>
                 </div>
            </div>
-         </div>
+         
         </div>
       
     </section>
