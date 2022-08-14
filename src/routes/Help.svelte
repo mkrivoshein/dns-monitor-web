56f1e4bd61be526a987da929b43c26e2b1357ea8
AlisaPekareva <69_alisa@list.ru>
2022-08-14 17:42:57 +0200

    #9 and 10,11Routing, sidebar,navbar, pages

---
+++ src/routes/Help.svelte
@@ -0,0 +1,19 @@
+ <h1 class="py-4 font-bold text-4xl text-blueGray-900">Query DNS records</h1> 
+    <div class="relative block px-4 pt-4 pb-8  h-300-px">
+        <h1 class="text-4xl text-center font-black">Help</h1>
+        
+        <div class="relative block sm:mx-3 px-3 py-1 ">
+            <div class="flex flex-wrap mt-1">
+                <div class="w-full mb-2">
+                    <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded">
+                        
+                                <div class="relative w-full  px-0 max-w-full flex-grow flex-1">
+                                    <h3 class="font-semibold text-lg text-blueGray-700">
+                                        Help
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                 </div>
+             </div>
+             </div>
