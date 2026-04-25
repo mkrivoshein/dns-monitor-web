b0f3ed33f3503434dce53f76572bf3d06e105af4
Mikhail Krivoshein <34556167+mkrivoshein@users.noreply.github.com>
2026-04-25 11:41:34 +0100

    Enhancements in UI

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { Auth } from "$components/Auth.class";
+    import { authStore } from "../store";
+    import keycloak_config from '../keycloak.json'
+    import Nav from "../components/Nav.svelte";
+    onMount(() => {
+        authStore.set(
+            new Auth(keycloak_config)
+        );
+        console.log('Inside +layout.onMount()');
+    });
+    export let segment: string;
+</script>
+
+<style>
+    main {
+        position: relative;
+        max-width: 56em;
+        background-color: white;
+        padding: 2em;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
+</style>
+
+<Nav {segment} />
+
+<main>
+    <slot />
+</main>
